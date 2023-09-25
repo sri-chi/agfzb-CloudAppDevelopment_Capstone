85a19daf0635dc8380da3746091d89561b985a6b
srikanth <chinanagari@gmail.com>
2023-09-25 02:11:37 -0400

    week 1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+ path(route='', view=views.about, name='about'),
     # path for contact us view
-
+ path(route='', view=views.contact, name='contact'),
     # path for registration
 
     # path for login
